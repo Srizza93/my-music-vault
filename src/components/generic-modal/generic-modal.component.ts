26a163086c3f00a4d8a635f27b0cf4d94c93d45b
Simone Rizza <simonerizza@macbookpro.home>
2025-04-20 01:56:57 +0200

    feat(my-music-vault): accessibility

--- src/components/generic-modal/generic-modal.component.ts
+++ src/components/generic-modal/generic-modal.component.ts
@@ -1,7 +1,15 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { AccessibilityHelper } from '@/helpers/accessibility.helper';
 
 @Component({
   selector: 'generic-modal',
@@ -13,7 +21,25 @@ export class GenericModalComponent {
   @Input() modalTitle: string = '';
   @Output() closeEvent = new EventEmitter<void>();
 
+  @ViewChild('modalContent') modalContentElement!: ElementRef<HTMLElement>;
+
+  constructor(private accessibilityHelper: AccessibilityHelper) {
+    this.accessibilityHelper = accessibilityHelper;
+  }
+
   closeModal() {
     this.closeEvent.emit();
+  }
+
+  ngAfterViewInit() {
+    if (this.modalContentElement) {
+      this.accessibilityHelper.trapFocus(
+        this.modalContentElement.nativeElement
+      );
+    }
+  }
+
+  ngOnDestroy() {
+    this.accessibilityHelper.loseFocus();
   }
 }

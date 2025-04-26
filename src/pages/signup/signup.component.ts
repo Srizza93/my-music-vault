4941a1e7e5221df7c0cdf9abea14c6d461fd0769
Simone Rizza <simonerizza@macbookpro.home>
2025-04-27 01:02:15 +0200

    fix(bug): fix translations warning

--- src/pages/signup/signup.component.ts
+++ src/pages/signup/signup.component.ts
@@ -6,7 +6,6 @@ import {
 } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { AuthFormComponent } from '@/components/auth-form/auth-form.component';
-import { AuthFormLabel } from '@/types/auth-form.interface';
 import { FormGroup } from '@angular/forms';
 import { AuthApi } from '@/api/auth.api';
 import { ToasterService, ToastType } from '@/services/toaster.service';
@@ -29,33 +28,6 @@ export class SignupComponent {
   ) {}
 
   emailSignedup: string | null = null;
-
-  get authFormLabels(): AuthFormLabel {
-    return {
-      signupButtonLabel: this.translate.instant('signup--button'),
-      signupAccessButtonLabel: this.translate.instant('signup-access--button'),
-      emailLabel: this.translate.instant('email--label'),
-      emailPlaceholder: this.translate.instant('email--placeholder'),
-      emailRequiredLabel: this.translate.instant('email-required--label'),
-      emailErrorLabel: this.translate.instant('email-error--label'),
-      passwordLabel: this.translate.instant('password--label'),
-      passwordPlaceholder: this.translate.instant('password--placeholder'),
-      passwordRequiredLabel: this.translate.instant('password-required--label'),
-      passwordPatternErrorLabel: this.translate.instant(
-        'password-pattern-error--label'
-      ),
-      confirmPasswordLabel: this.translate.instant('confirm-password--label'),
-      confirmPasswordPlaceholder: this.translate.instant(
-        'confirm-password--placeholder'
-      ),
-      confirmPasswordRequiredLabel: this.translate.instant(
-        'confirm-password-required--label'
-      ),
-      confirmPasswordMismatchLabel: this.translate.instant(
-        'confirm-password-mismatch--label'
-      ),
-    };
-  }
 
   signup(signupForm: FormGroup) {
     if (signupForm.invalid) return;
